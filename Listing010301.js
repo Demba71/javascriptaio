303c10273d0e7421a0ffca1cd84f41d2c60de6c0
Ivan Chin <ijchin@gmail.com>
2024-04-04 14:49:29 -0600

    Added Listing010301 - template literal notation

---
+++ Listing010301.js
@@ -0,0 +1,6 @@
+let orderTotal = 39.99;
+let itemPurchased = "JavaScript All-in-One For Dummies";
+let customer = "Joe Q. Developer";
+
+let thankYou = `${customer}, thank you for your order of ${itemPurchased}. Your payment of ${orderTotal} was successful.`;
+console.log(thankYou);
